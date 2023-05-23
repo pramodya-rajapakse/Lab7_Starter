550e9250022444b86a5b2e7d791421f85e4f81e2
pramodya-rajapakse <102259936+pramodya-rajapakse@users.noreply.github.com>
2023-05-23 14:54:42 -0700

    update sw.js

--- sw.js
+++ sw.js
@@ -28,7 +28,6 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  if (!(event.request.url.indexOf('http') === 0)) return;
   // We added some known URLs to the cache above, but tracking down every
   // subsequent network request URL and adding it manually would be very taxing.
   // We will be adding all of the resources not specified in the intiial cache
